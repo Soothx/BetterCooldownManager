43bfec8a3e7fcefbca3c704e987b2acabb50a26b
IskrenTs <106561107+IskrenTs@users.noreply.github.com>
2026-01-26 00:46:27 +0200

    Add Elemental Shaman too

--- Modules/PowerBar.lua
+++ Modules/PowerBar.lua
@@ -44,6 +44,7 @@ local function DetectSecondaryPower()
     elseif class == "DEMONHUNTER" then
         if specID == 1480 then return true end
     elseif class == "SHAMAN" then
+        if specID == 262 then return true end
         if specID == 263 then return true end
     elseif class == "PRIEST" then
         if specID == 258 then return true end    
